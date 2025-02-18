22b3597436127d2bdb1a1f112233c92d063f70af
Alex <ale.borges8@me.com>
2025-02-17 23:09:59 -0300

    fix trust host

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -31,6 +31,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
+  trustHost: true,
   providers: [
     DiscordProvider,
     /**
