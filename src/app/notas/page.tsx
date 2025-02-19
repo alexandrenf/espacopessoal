61892c8e5151361d2c7ce7d4a7d7a0b06f87c7b0
Alex <ale.borges8@me.com>
2025-02-19 14:18:45 -0300

    added blank page to make builder happy

---
+++ src/app/notas/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotasPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Notas Page</h1>
+        </div>
+    );
+};
+
+export default NotasPage;
