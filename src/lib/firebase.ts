226990a006ab200e9892676831cd662e4af9f8d6
Alex <ale.borges8@me.com>
2025-03-08 02:41:51 -0300

    FIXING A ERROR THAT SHOULD NOT EXIST AND WAITING TO BUILD AAA

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import type { FirebaseApp } from "firebase/app";
 import type { Messaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBec6lEqs4sv6h7JcyeL1LuELZLzknz5u4",
+  authDomain: "espacopessoal-6f167.firebaseapp.com",
+  projectId: "espacopessoal-6f167",
+  storageBucket: "espacopessoal-6f167.firebasestorage.app",
+  messagingSenderId: "428339453220",
+  appId: "1:428339453220:web:a301151e39b56e77e6a7b1",
+  measurementId: "G-YE68R8GDE8"
 };
 
 let messagingInstance: Messaging | null = null;
