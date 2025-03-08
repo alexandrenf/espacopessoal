204e99d4fd6fc27cffcfd65a92ea11c7858c1f55
Alex <ale.borges8@me.com>
2025-03-08 11:39:22 -0300

    test2

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { type Metadata, type Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Página não encontrada",
+  description: "A página que você está procurando não existe.",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
