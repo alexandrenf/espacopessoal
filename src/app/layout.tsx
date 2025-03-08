e14a6f25c6c964b36f5f4f5eec247b51ca5e2169
Alex Filho <5628895+alexandrenf@users.noreply.github.com>
2025-03-08 11:39:22 -0300

    test2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { type Metadata, type Viewport } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { Toaster } from 'sonner';
@@ -12,22 +12,23 @@ export const metadata: Metadata = {
   title: "Espaço Pessoal",
   description: "Organize suas notas, pensamentos e ideias em um único lugar seguro e acessível de qualquer lugar.",
   manifest: "/manifest.json",
-  themeColor: "#000000",
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
     title: "Espaço Pessoal",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
   icons: [
     { rel: "icon", url: "/favicon.ico" },
     { rel: "apple-touch-icon", url: "/icons/icon-192x192.png" },
   ],
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
