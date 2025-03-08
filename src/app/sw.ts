26ea0361e2c93ec64d243e6f9730ff1d37ec107f
Alex Filho <5628895+alexandrenf@users.noreply.github.com>
2025-03-08 11:58:55 -0300

    pwa try4

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,43 +1,39 @@
 declare global {
-    interface Window {
-      workbox: {
-        addEventListener: (event: string, callback: (event: Event) => void) => void;
-        messageSkipWaiting: () => void;
-        register: () => void;
-      };
-    }
+  interface Window {
+    workbox: {
+      addEventListener: (event: string, callback: (event: Event) => void) => void;
+      messageSkipWaiting: () => void;
+      register: () => void;
+    };
   }
-  
-  export function registerServiceWorker() {
-    if (
-      typeof window !== 'undefined' &&
-      'serviceWorker' in navigator &&
-      window.workbox !== undefined
-    ) {
-      const wb = window.workbox;
-      
-      // Add event listeners to handle PWA lifecycle
-      wb.addEventListener('installed', event => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
-  
-      wb.addEventListener('controlling', event => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
-  
-      wb.addEventListener('activated', event => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
-  
-      // Send skip waiting event to ServiceWorker
-      wb.addEventListener('waiting', event => {
-        wb.messageSkipWaiting();
-      });
-  
-      // Register the ServiceWorker
-      wb.register();
-    }
+}
+
+export function registerServiceWorker() {
+  if (
+    typeof window !== 'undefined' &&
+    'serviceWorker' in navigator &&
+    window.workbox !== undefined
+  ) {
+    const wb = window.workbox;
+    
+    // Add event listeners to handle PWA lifecycle
+    wb.addEventListener('installed', event => {
+      console.log(`Event ${event.type} is triggered.`);
+      console.log(event);
+    });
+
+    wb.addEventListener('controlling', event => {
+      console.log(`Event ${event.type} is triggered.`);
+      console.log(event);
+    });
+
+    wb.addEventListener('activated', event => {
+      console.log(`Event ${event.type} is triggered.`);
+      console.log(event);
+    });
+
+    // Force the waiting service worker to become the active service worker
+    wb.messageSkipWaiting();
+    wb.register();
   }
+}
