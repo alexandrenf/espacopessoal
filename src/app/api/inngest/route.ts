d6cf3c3121fb9e9ffe919defe6673050114b7d2d
Alex Filho <5628895+alexandrenf@users.noreply.github.com>
2025-03-11 15:02:33 -0300

    test deno1

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from 'inngest/next';
-import { inngest } from '~/server/inngest/client';
-import { processNotifications } from '~/server/inngest/notifications';
-
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [processNotifications],
-});
