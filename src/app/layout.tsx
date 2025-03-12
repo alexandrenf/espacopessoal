327791a5555829562b90d4238df4e83f00b6da38
Alex Filho <5628895+alexandrenf@users.noreply.github.com>
2025-03-12 20:28:51 -0300

    final touches

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,14 @@ export const metadata: Metadata = {
   },
   icons: [
     { rel: "icon", url: "/favicon.ico" },
-    { rel: "apple-touch-icon", url: "/icons/icon-192x192.png" },
+    { rel: "apple-touch-icon", url: "/icons/icon-192x192.png", sizes: "192x192" },
+    { rel: "apple-touch-icon", url: "/icons/icon-512x512.png", sizes: "512x512" },
+    { rel: "apple-touch-icon", url: "/icons/icon-152x152.png", sizes: "152x152" },
+    { rel: "apple-touch-icon", url: "/icons/icon-144x144.png", sizes: "144x144" },
+    { rel: "apple-touch-icon", url: "/icons/icon-128x128.png", sizes: "128x128" },
+    { rel: "icon", url: "/icons/icon-384x384.png", sizes: "384x384", type: "image/png" },
+    { rel: "icon", url: "/icons/icon-72x72.png", sizes: "72x72", type: "image/png" },
+    { rel: "icon", url: "/icons/icon-96x96.png", sizes: "96x96", type: "image/png" },
   ],
 };
 
