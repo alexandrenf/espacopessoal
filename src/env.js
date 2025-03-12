6147abbec81d147f6ee95c548d12c9c9e2cb5e72
alexandrenf <ale.borges8@me.com>
2025-03-11 23:12:37 -0300

    Update env.js

--- src/env.js
+++ src/env.js
@@ -2,11 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 // Set DATABASE_URL and NEXTAUTH_URL based on NODE_ENV before env validation
-if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL_PRODUCTION
-    : process.env.DATABASE_URL_DEVELOPMENT;
-}
 
 if (!process.env.NEXTAUTH_URL) {
   process.env.NEXTAUTH_URL = process.env.NODE_ENV === "production"
@@ -26,11 +21,9 @@ export const env = createEnv({
         : z.string().optional(),
     AUTH_DISCORD_ID: z.string(),
     AUTH_DISCORD_SECRET: z.string(),
-    DATABASE_URL_DEVELOPMENT: z.string().url(),
-    DATABASE_URL_PRODUCTION: z.string().url(),
+    DATABASE_URL: z.string().url(),
     NEXTAUTH_URL_DEVELOPMENT: z.string().url(),
     NEXTAUTH_URL_PRODUCTION: z.string().url(),
-    DATABASE_URL: z.string().url(),
     NEXTAUTH_URL: z.string().url(),
     EMAIL_SERVER: z.string().url(),
     EMAIL_FROM: z.string().email(),
@@ -39,7 +32,14 @@ export const env = createEnv({
       .default("development"),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    FIREBASE_ADMIN_PROJECT_ID: z.string(),
+    FIREBASE_ADMIN_CLIENT_EMAIL: z.string(),
+    FIREBASE_ADMIN_PRIVATE_KEY: z.string(),
+    FIREBASE_FCM_VAPID_KEY: z.string(),
+    DENO_API_KEY: z.string(),
+    DENO_API_URL: z.string().url(),
   },
+
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -58,8 +58,6 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
-    DATABASE_URL_DEVELOPMENT: process.env.DATABASE_URL_DEVELOPMENT,
-    DATABASE_URL_PRODUCTION: process.env.DATABASE_URL_PRODUCTION,
     NEXTAUTH_URL_DEVELOPMENT: process.env.NEXTAUTH_URL_DEVELOPMENT,
     NEXTAUTH_URL_PRODUCTION: process.env.NEXTAUTH_URL_PRODUCTION,
     DATABASE_URL: process.env.DATABASE_URL,
@@ -69,6 +67,12 @@ export const env = createEnv({
     EMAIL_FROM: process.env.EMAIL_FROM,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    FIREBASE_ADMIN_CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    FIREBASE_FCM_VAPID_KEY: process.env.FIREBASE_FCM_VAPID_KEY,
+    DENO_API_KEY: process.env.DENO_API_KEY,
+    DENO_API_URL: process.env.DENO_API_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
